c1e6bfa28610415bec7783a8522bac9b443ea858
jasonymk97 <jasonymk97@gmail.com>
2024-04-25 18:15:26 +0800

    Implement setProxy.js for api proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,36 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        '/blizzard-api',
+        createProxyMiddleware({
+            target: 'https://us.api.blizzard.com',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/blizzard-api': '',
+            },
+        })
+    );
+
+    app.use(
+        '/github-api',
+        createProxyMiddleware({
+            target: 'https://api.github.com',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/github-api': '',
+            },
+        })
+    );
+
+    app.use(
+        '/google-api',
+        createProxyMiddleware({
+            target: 'https://www.googleapis.com',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/google-api': '',
+            },
+        })
+    );
+};
